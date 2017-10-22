0c6125a1663281e91c53fbc05203c6e8025bb3b2
Hmc-cs-tdubno <tdubno@cs.hmc.edu>
2017-10-22 11:32:33 -0700

    finally fixed json issues

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,13 +12,11 @@ class Person < ApplicationRecord
 				Person.create! curhash
 			end
 		elsif(file.content_type=="application/json")
-			curfile = File.read(file.path)
-			print(curfile)
-			puts curfile.class()
-			# data_hash = curfile.to_hash
-			# datahash["extra"] = {
-		# }
-			Person.create! eval(curfile)
+			curfile = file.read
+			hash = JSON.parse(curfile)
+		
+		
+			Person.create! hash
 		else
 			puts "hey stop that"
 		end
