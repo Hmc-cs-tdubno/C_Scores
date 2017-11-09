0e302d3b349a4b80d5b40b3737ebd8e154553393
hmc-cs-fsouth <fsoutherland@hmc.edu>
2017-11-09 15:37:35 -0800

    added predictions view

--- app/models/person.rb
+++ app/models/person.rb
@@ -28,7 +28,7 @@ class Person < ApplicationRecord
             		:contributor => curhash["contributor"]
         			}
         			curhash["style"] = styles.max_by{|k,v| v}[0]
-							put curhash["style"]
+					puts curhash["style"]
 					#setting extra equal to empty hash for testing purposes
 					curhash["extra"] = {}
 					message	= "CSV uploaded"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'predictions/index'
+
   devise_for :users
     resources :uploads do
       collection {post :import}
