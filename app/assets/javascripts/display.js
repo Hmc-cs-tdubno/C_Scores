11087ec3a1affba0574f6d651e09b82b17849b64
hmc-cs-gwomark <gwomark@cs.hmc.edu>
2017-11-30 01:27:21 -0800

    added comments and cleaned up graph code(ruby and js).

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -1,13 +1,10 @@
 var margin = {top: 20, right: 70, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom,
-    padding = 50;
+    width = 625 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom,
+    padding = 25;
+
 
  function drawBarGraph () {
-    // set the ranges
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-    var y = d3.scale.linear().range([height, 0]);
 
     // add the SVG element
     var svg = d3.select("#bargraph")
@@ -18,38 +15,47 @@ var margin = {top: 20, right: 70, bottom: 70, left: 40},
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
-    // Keep track of largest frequency for setting axis ticks
-    var maxfreq = 0
+    // Set the scales for the axes
+    var x = d3.scale.ordinal().rangeRoundBands([padding, width-padding], .05);
+    var y = d3.scale.linear().range([height-padding, padding]);
 
-    var data = d3.json("http://localhost:3000/api/bar",function(data){
-      data.data.forEach(function(d){
+    d3.json("http://localhost:3000/api/bar",function(data){
+      // Keep track of largest frequency for setting axis ticks
+      var maxfreq = 0
+
+      data = data.data
+      // Cast data to be digestable by d3
+      data.forEach(function(d){
         d.style = d.style;
         d.freq = +d.freq;
         if (d.freq > maxfreq) {
           maxfreq = d.freq
         }
       })
-      // scale the range of the data
-      x.domain(data.data.map(function(d) { return d.style; }));
-      y.domain([0, d3.max(data.data, function(d) { return d.freq; })]);
 
-      // define the axis
+      // scale the range of the data
+      x.domain(data.map(function(d) { return d.style; }));
+      y.domain([0, d3.max(data, function(d) { return d.freq; })]);
+
+      // Create new axis object
       var xAxis = d3.svg.axis()
           .scale(x)
           .orient("bottom")
 
-      var formatyAxis = d3.format('.0f')
-
+      // Create new axis object
       var yAxis = d3.svg.axis()
           .scale(y)
           .orient("left")
-          .tickFormat(formatyAxis)
-          .ticks(maxfreq/4);
+          .tickFormat(barYAxisFormat())
 
-      // add axis
+      if (maxfreq < 10) {
+        yAxis = yAxis.ticks(maxfreq)
+      }
+
+      // Add x-axis to chart
       svg.append("g")
           .attr("class", "x-axis-bar axis")
-          .attr("transform", "translate(0," + height + ")")
+          .attr("transform", "translate(0," + (height-padding) + ")")
           .call(xAxis)
         .selectAll("text")
           .style("text-anchor", "end")
@@ -57,27 +63,28 @@ var margin = {top: 20, right: 70, bottom: 70, left: 40},
           .attr("dy", ".55em")
           .attr("transform", "rotate(0)" );
 
+      // Add y-axis to chart
       svg.append("g")
           .attr("class", "y-axis-bar axis")
+          .attr("transform", "translate("+(padding)+",0)")
           .call(yAxis)
         .append("text")
           .attr("transform", "rotate(-90)")
-          .attr("y", 0 - margin.left)
-          .attr("x",0 - (height / 2))
+          .attr("y", 0 - padding - margin.left)
+          .attr("x",0 - ((height-padding) / 2))
           .attr("dy", "1em")
           .style("text-anchor", "middle")
           .text("Frequency");
 
-
-      // Add bar chart
+      // Add bars to chart
       svg.selectAll("bar")
-          .data(data.data)
+          .data(data)
         .enter().append("rect")
           .attr("class", "bar")
           .attr("x", function(d) { return x(d.style); })
           .attr("width", x.rangeBand())
           .attr("y", function(d) { return y(d.freq); })
-          .attr("height", function(d) { return height - y(d.freq); });
+          .attr("height", function(d) { return (height-padding) - y(d.freq); });
 
     });
 }
@@ -85,12 +92,18 @@ var margin = {top: 20, right: 70, bottom: 70, left: 40},
 function updateBarGraph() {
   var url = d3UpdateUrl("http://localhost:3000/api/bar?");
 
+  // Set the scales for the axes
+  var x = d3.scale.ordinal().rangeRoundBands([padding, width-padding], .05);
+  var y = d3.scale.linear().range([height-padding, padding]);
+
   // Get the data again
   d3.json(url, function(error, data) {
     // Keep track of largest frequency for setting axis ticks
     var maxfreq = 0
-    console.log(data.data)
-    data.data.forEach(function(d){
+
+    data = data.data
+    // Cast data to be digestable by d3
+    data.forEach(function(d){
       d.style = d.style;
       d.freq = +d.freq;
       if (d.freq > maxfreq) {
@@ -98,23 +111,24 @@ function updateBarGraph() {
       }
     })
 
-    // set the ranges
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-    var y = d3.scale.linear().range([height, 0]);
 
-  	// Scale the range of the data again
-    x.domain(data.data.map(function(d) { return d.style; }));
-    y.domain([0, d3.max(data.data, function(d) { return d.freq; })]);
+    // Update the domains of the x and y scales
+    x.domain(data.map(function(d) { return d.style; }));
+    y.domain([0, d3.max(data, function(d) { return d.freq; })]);
 
 
-    // Select the section we want to apply our changes to
     var svg = d3.select("#bargraph");
 
-    // KEEP X AXIS the same
+    // // // // // // // // //
+    //      UPDATE AXES     //
+    // // // // // // // // //
+
+    // Create new axis object
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom");
 
+    // Add x-axis to graph
     svg.select(".x-axis-bar")
       .call(xAxis).selectAll("text")
         .style("text-anchor", "end")
@@ -122,29 +136,29 @@ function updateBarGraph() {
         .attr("dy", ".55em")
         .attr("transform", "rotate(0)" );
 
-    // UPDATE Y AXIS
-    var formatyAxis = d3.format('.0f')
-
+    // Create new axis object
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left")
-        .tickFormat(formatyAxis);
+        .tickFormat(barYAxisFormat());
 
+    // If there are less than 10 datapoints for
+    // a style use less than 10 ticks
+    // otherwise use 10 ticks
     if (maxfreq < 10) {
       yAxis = yAxis.ticks(maxfreq)
     }
 
-
+    // Add y-axis to graph and do nice animation
     svg.select(".y-axis-bar").transition()
       .duration(750)
       .call(yAxis);
 
     // // // // // // // // //
-    // UPDATE SOME BARS YO //
+    //      UPDATE BARS     //
     // // // // // // // // //
 
-    var bar = svg.selectAll(".bar").data(data.data)
-
+    var bar = svg.selectAll(".bar").data(data)
 
     // Add new bars
     bar.enter().append("rect")
@@ -152,7 +166,7 @@ function updateBarGraph() {
       .attr("x", function(d) { return x(d.style); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.freq); })
-      .attr("height", function(d) { return height - y(d.freq); });
+      .attr("height", function(d) { return (height-padding) - y(d.freq); });
 
     // Remove old bars
     bar.exit().remove();
@@ -161,7 +175,7 @@ function updateBarGraph() {
     bar.transition()
       .duration(750)
       .attr("y", function(d) { return y(d.freq); })
-      .attr("height", function(d) { return height - y(d.freq); });
+      .attr("height", function(d) { return (height-padding) - y(d.freq); });
   });
 }
 
@@ -172,9 +186,8 @@ function drawScatterGraph() {
     var style2 = $('#style2').val().toLowerCase();
 
 
-    //Create url for grabing data
+    // Create url for grabing data
     var url = d3UpdateUrl("http://localhost:3000/api/scatter?")
-    console.log(url)
     if (url != "http://localhost:3000/api/scatter") {
       url = url + "&style1="+style1+"&style2="+style2
     } else {
@@ -182,10 +195,10 @@ function drawScatterGraph() {
     }
 
     var x = d3.scale.linear()
-        .range([0, width]);
+        .range([padding, width-padding]);
 
     var y = d3.scale.linear()
-        .range([height, 0]);
+        .range([height-padding, padding]);
 
     var color = d3.scale.category10();
 
@@ -197,7 +210,9 @@ function drawScatterGraph() {
         .scale(y)
         .orient("left");
 
-    var svg2 = d3.select("#graphdiv").append("svg").attr("id", "scattergraph")
+
+    // Add svg element to page
+    var svg = d3.select("#graphdiv").append("svg").attr("id", "scattergraph")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .call(responsivefy)
@@ -219,10 +234,10 @@ function drawScatterGraph() {
       y.domain(d3.extent(data, function(person) { return person.style2; })).nice();
 
 
-
-      svg2.append("g")
+      // Add x-axis to scatterplot
+      svg.append("g")
           .attr("class", "x-axis-scatter axis")
-          .attr("transform", "translate(20," + height + ")")
+          .attr("transform", "translate(0," + (height-padding) + ")")
           .call(xAxis)
         .append("text")
           .attr("class", "label")
@@ -230,19 +245,20 @@ function drawScatterGraph() {
           .style("text-anchor", "middle")
           .text(style1);
 
-      svg2.append("g")
+      // add y-axis to scatterplot
+      svg.append("g")
           .attr("class", "y-axis-scatter axis")
-          .attr("transform", "translate(20,0)")
+          .attr("transform", "translate("+padding+",0)")
           .call(yAxis)
         .append("text")
           .attr("transform", "rotate(-90)")
-          .attr("y", 0 - margin.left - 10)
+          .attr("y", (-1*padding) -margin.left)
           .attr("x",0 - (height / 2))
           .attr("dy", "1em")
           .style("text-anchor", "middle")
           .text(style2)
 
-      svg2.selectAll(".dot")
+      svg.selectAll(".dot")
           .data(data)
         .enter().append("circle")
           .attr("class", "dot")
@@ -253,64 +269,8 @@ function drawScatterGraph() {
             return y(person.style2); })
           .style("fill", "black")
 
-
-      ////////////////////
-      // TRENDLINE CODE //
-      ////////////////////
-
-      // var lg = calcLinear(data, "style1", "style2", d3.min(data, function(d){ return d.style1}), d3.min(data, function(d){ return d.style2}));
-      // console.log(lg)
-      // svg2.append("line")
-	    //     .attr("class", "regression")
-	    //     .attr("x1", x(lg.ptA.x))
-	    //     .attr("y1", y(lg.ptA.y))
-	    //     .attr("x2", x(lg.ptB.x))
-	    //     .attr("y2", y(lg.ptB.y));
-
-      // // Create format for equation
-      // var decimalFormat = d3.format("0.2f");
-      //
-      // var xLabels = d3.extent(data, function(person) { return person.style1; })
-      //
-      // var xSeries = d3.range(1, xLabels.length + 1);;
-      // var ySeries = data.map(function(person) { return person.style2 });
-      //
-      // var leastSquaresCoeff = leastSquares(xSeries, ySeries);
-      //
-      // var x1 = xLabels[0];
-		  // var y1 = leastSquaresCoeff[0] + leastSquaresCoeff[1];
-		  // var x2 = xLabels[xLabels.length - 1];
-		  // var y2 = leastSquaresCoeff[0] * xSeries.length + leastSquaresCoeff[1];
-		  // var trendData = [[x1,y1,x2,y2]];
-      //
-      // var trendline = svg2.selectAll(".trendline")
-			// .data(trendData);
-      //
-  		// trendline.enter()
-  		// 	.append("line")
-  		// 	.attr("class", "trendline")
-  		// 	.attr("x1", function(d) { return x(d[0]); })
-  		// 	.attr("y1", function(d) { return y(d[1]); })
-  		// 	.attr("x2", function(d) { return x(d[2]); })
-  		// 	.attr("y2", function(d) { return y(d[3]); })
-  		// 	.attr("stroke", "black")
-  		// 	.attr("stroke-width", 1);
-      //
-  		// // display equation on the chart
-  		// svg2.append("text")
-  		// 	.text("eq: " + decimalFormat(leastSquaresCoeff[0]) + "x + " +
-  		// 		decimalFormat(leastSquaresCoeff[1]))
-  		// 	.attr("class", "text-label")
-  		// 	.attr("x", function(d) {return x(x2) - 60;})
-  		// 	.attr("y", function(d) {return y(y2) - 30;});
-      //
-  		// // display r-square on the chart
-  		// svg2.append("text")
-  		// 	.text("r-sq: " + decimalFormat(leastSquaresCoeff[2]))
-  		// 	.attr("class", "text-label")
-  		// 	.attr("x", function(d) {return x(x2) - 60;})
-  		// 	.attr("y", function(d) {return y(y2) - 10;});
-
+    // Make sure the new scatter plot does not
+    // appear while user is looking at bar graph
     if ($('#bargraph').is(":visible")) {
       $('#scattergraph').hide();
     }
@@ -320,7 +280,8 @@ function drawScatterGraph() {
 
 // BROKEN FUNCTION: Weirdly places data points in the wrong place
 // function updateScatterGraph() {
-//   [style1, style2] = getStyles()
+//   var style1 = $('#style1').val().toLowerCase();
+//   var style2 = $('#style2').val().toLowerCase();
 //
 //   //Create url for grabing data
 //   var url = d3UpdateUrl("http://localhost:3000/api/scatter?")
@@ -332,7 +293,6 @@ function drawScatterGraph() {
 //
 //   d3.json(url, function(error, data) {
 //     if (error) throw error;
-//     console.log(data)
 //     data = data.data
 //     data.forEach(function(person) {
 //       person.style1 = +person.style1;
@@ -342,10 +302,10 @@ function drawScatterGraph() {
 //
 //     // Scale the axes
 //     var x = d3.scale.linear()
-//         .range([0, width]);
+//         .range([padding, width-padding]);
 //
 //     var y = d3.scale.linear()
-//         .range([height, 0]);
+//         .range([height-padding, padding]);
 //
 //     // create axis objects
 //     var xAxis = d3.svg.axis()
@@ -365,6 +325,7 @@ function drawScatterGraph() {
 //     // Update Axes
 //     svg.select(".x-axis-scatter").transition()
 //       .duration(1000)
+//       .attr("transform", "translate(0," + (height-padding) + ")")
 //       .call(xAxis);
 //
 //     svg.select(".y-axis-scatter").transition()
@@ -379,7 +340,7 @@ function drawScatterGraph() {
 //     // Add new dots
 //     dots.enter().append("circle")
 //         .attr("class", "dot")
-//         .attr("r", 5)
+//         .attr("r", 2)
 //         .attr("cx", function(person) {
 //           return x(person.style1); })
 //         .attr("cy", function(person) {
@@ -478,9 +439,9 @@ function d3UpdateUrl(url) {
   return url.slice(0, -1);
 }
 
-// Code borrowed from Brendan Sudol
-// link: https://brendansudol.com/writing/responsive-d3
-// It allows us to make our graphs respond to the size of the page
+// // Code borrowed from Brendan Sudol
+// // link: https://brendansudol.com/writing/responsive-d3
+// // It allows us to make our graphs respond to the size of the page
 function responsivefy(svg) {
     // get container + svg aspect ratio
     var container = d3.select(svg.node().parentNode),
@@ -508,102 +469,13 @@ function responsivefy(svg) {
     }
 }
 
-// returns slope, intercept and r-square of the line
-function leastSquares(xSeries, ySeries) {
-	var reduceSumFunc = function(prev, cur) { return prev + cur; };
-
-	var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
-	var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
-
-	var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
-		.reduce(reduceSumFunc);
-
-	var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
-		.reduce(reduceSumFunc);
-
-	var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
-		.reduce(reduceSumFunc);
-
-	var slope = ssXY / ssXX;
-	var intercept = yBar - (xBar * slope);
-	var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
-
-	return [slope, intercept, rSquare];
+function barYAxisFormat() {
+  return d3.format('.0f')
 }
 
-function calcLinear(data, x, y, minX, minY){
-   /////////
-   //SLOPE//
-   /////////
 
-   // Let n = the number of data points
-   var n = data.length;
 
-   // Get just the points
-   var pts = [];
-   data.forEach(function(d,i){
-     var obj = {};
-     obj.x = d[x];
-     obj.y = d[y];
-     obj.mult = obj.x*obj.y;
-     pts.push(obj);
-   });
 
-   // Let a equal n times the summation of all x-values multiplied by their corresponding y-values
-   // Let b equal the sum of all x-values times the sum of all y-values
-   // Let c equal n times the sum of all squared x-values
-   // Let d equal the squared sum of all x-values
-   var sum = 0;
-   var xSum = 0;
-   var ySum = 0;
-   var sumSq = 0;
-   pts.forEach(function(pt){
-     sum = sum + pt.mult;
-     xSum = xSum + pt.x;
-     ySum = ySum + pt.y;
-     sumSq = sumSq + (pt.x * pt.x);
-   });
-   var a = sum * n;
-   var b = xSum * ySum;
-   var c = sumSq * n;
-   var d = xSum * xSum;
-
-   // Plug the values that you calculated for a, b, c, and d into the following equation to calculate the slope
-   // slope = m = (a - b) / (c - d)
-   var m = (a - b) / (c - d);
-
-   /////////////
-   //INTERCEPT//
-   /////////////
-
-   // Let e equal the sum of all y-values
-   var e = ySum;
-
-   // Let f equal the slope times the sum of all x-values
-   var f = m * xSum;
-
-   // Plug the values you have calculated for e and f into the following equation for the y-intercept
-   // y-intercept = b = (e - f) / n
-   var b = (e - f) / n;
-
-   // Print the equation below the chart
-   // document.getElementsByClassName("equation")[0].innerHTML = "y = " + m + "x + " + b;
-   // document.getElementsByClassName("equation")[1].innerHTML = "x = ( y - " + b + " ) / " + m;
-
-   // return an object of two points
-   // each point is an object with an x and y coordinate
-   return {
-     ptA : {
-       x: minX,
-       y: m * minX + b
-     },
-     ptB : {
-       y: minY,
-       x: (minY - b) / m
-     }
-   }
-
- }
 
 // BROKEN FUNCTION: returns html elements instead of strings because of god knows why
 // function getStyles() {
